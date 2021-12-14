5b32356ca577fcd3511e1a87259279c2c7fa89c0
Toma <TomaSajt@users.noreply.github.com>
2021-12-14 16:25:14 +0100

    :))

---
+++ 2021/13/Day13/Day13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+var board = new HashSet<(int x, int y)>();
+using (var sr = new StreamReader("input.txt"))
+{
+    string l;
+    while (true)
+    {
+        l = sr.ReadLine();
+        if (l == "") break;
+        board.Add((int.Parse(l.Split(',')[0]), int.Parse(l.Split(',')[1])));
+    }
+    while (!sr.EndOfStream)
+    {
+        l = sr.ReadLine();
+        Fold(int.Parse(l[13..]), l[11] == 'x');
+    }
+}
+Log();
+
+void Fold(int pos, bool alongX)
+{
+    var old = board;
+    board = new HashSet<(int, int)>();
+    foreach (var (x, y) in old)
+    {
+        if (alongX) board.Add((pos - Math.Abs(x - pos), y));
+        else board.Add((x, pos - Math.Abs(y - pos)));
+    }
+}
+void Log()
+{
+    int w = board.Max(t => t.x), h = board.Max(t=>t.y);
+    var sb = new StringBuilder();
+    for (int j = 0; j <= h; j++)
+    {
+        for (int i = 0; i <= w; i++)
+        {
+            sb.Append(board.Contains((i, j)) ? "#" : ".");
+        }
+        sb.AppendLine();
+    }
+    Console.WriteLine(sb);
+}
---
+++ 2021/14/Day14/Day14/Program.cs
@@ -0,0 +1,26 @@
+﻿var fileS = File.ReadAllText("input.txt").Trim().Split("\r\n\r\n");
+var poly = fileS[0];
+var pairCounts = new Dictionary<string, long>();
+var map = fileS[1].Split("\r\n").ToDictionary(k => k[0..2], k => k[6]);
+void Inc(string k, long v)
+{
+    if (!pairCounts.TryAdd(k, v)) pairCounts[k] += v;
+}
+for (int i = 0; i < poly.Length - 1; i++) Inc(poly[i..(i + 2)], 1);
+for (int i = 0; i < 40; i++)
+{
+    var old = pairCounts;
+    pairCounts = new Dictionary<string, long>();
+    foreach (var (pair, count) in old)
+    {
+        if (map.ContainsKey(pair))
+        {
+            Inc($"{pair[0]}{map[pair]}", count);
+            Inc($"{map[pair]}{pair[1]}", count);
+        }
+        else Inc(pair, count);
+    }
+}
+var letterCounts = pairCounts.GroupBy(a => a.Key[0]).ToDictionary(g => g.Key, g => g.Sum(a => a.Value));
+letterCounts[poly[^1]]++;
+Console.WriteLine(letterCounts.Max(kvp => kvp.Value) - letterCounts.Min(kvp => kvp.Value));
