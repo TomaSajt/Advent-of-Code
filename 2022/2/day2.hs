5c76ea82c214aa908d39cb3128155cd30599af9e
Toma <tomasajtofficial@gmail.com>
2022-12-02 09:52:45 +0100

    refactor 2022 day 2 haskell solution

--- 2022/2/day2.hs
+++ 2022/2/day2.hs
@@ -1,7 +1,7 @@
 import Data.Char (ord)
 
-toIDs :: [Char] -> [Int]
-toIDs [ca, cb] = [(ord ca) - (ord 'A'), (ord cb) - (ord 'X')]
+toIDs :: String -> [Int]
+toIDs [ca,_,cb] = [ord ca - ord 'A', ord cb - ord 'X']
 
 score :: [Int] -> Int
 score [a,b] = b + 1 + 3 * (mod (b - a + 1) 3)
@@ -11,8 +11,6 @@ score2 [a,o] = (mod (a + o - 1) 3) + 1 + 3 * o
 
 main :: IO()
 main = do
-    content <- readFile "input.txt"
-    let inp = map(toIDs . map head . words) . lines $ content
-    solve score inp
-    solve score2 inp
-    where solve sc = print . sum . map sc
+    inp <- fmap(map toIDs . lines) $ readFile "input.txt"
+    print $ sum . map score $ inp
+    print $ sum . map score2 $ inp
