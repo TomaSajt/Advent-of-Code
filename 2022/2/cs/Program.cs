03899b8e35fe8b5e756a76f209c29a437383d3e9
Toma <tomasajtofficial@gmail.com>
2022-12-02 12:04:21 +0100

    Add C# solution for 2022 day 2

---
+++ 2022/2/cs/Program.cs
@@ -0,0 +1,5 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var pairs = lines.Select(x => new { a = x[0] - 'A', b = x[2] - 'X' });
+var res1 = pairs.Sum(x => x.b + x.a + 3 * ((x.b - x.a + 4) % 3));
+var res2 = pairs.Sum(x => (x.a + x.b + 2) % 3 + 1 + 3 * x.b);
+Console.WriteLine($"{res1} {res2}");
