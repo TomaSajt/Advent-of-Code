598a92214b2ca5fa872b9bfba8753705b8260a17
Toma <tomasajtofficial@gmail.com>
2022-12-03 07:50:14 +0100

    refactor day 3 2022

--- 2022/3/day3.hs
+++ 2022/3/day3.hs
@@ -1,4 +1,4 @@
-import Data.List (intersect, nub, foldl1')
+import Data.List (intersect, foldl1')
 import Data.Char (ord)
 
 splitEvery :: Int -> [a] -> [[a]]
@@ -6,15 +6,14 @@ splitEvery _ [] = []
 splitEvery n xs = as : splitEvery n bs 
   where (as,bs) = splitAt n xs
 
-getBad :: String -> [Char]
-getBad ln = nub $ intersect a b
-    where (a,b) = splitAt (div (length ln) 2) ln
+splitHalf :: String -> (String, String)
+splitHalf s =  splitAt (div (length s) 2) s
 
 score :: Char -> Int
-score c = if elem c ['A'..'Z'] then 27 + ord c - ord 'A' else 1 + ord c - ord 'a'
+score c = ord c - if elem c ['A'..'Z'] then 38 else 96
 
 main :: IO()
 main = do
     lns <- fmap lines $ readFile "input.txt"
-    print $ sum . map score $ lns >>= getBad
-    print $ sum . map(score . head . foldl1' intersect) $ splitEvery 3 lns
+    print $ sum . map (score . head . uncurry intersect . splitHalf) $ lns
+    print $ sum . map (score . head . foldl1' intersect) $ splitEvery 3 lns
