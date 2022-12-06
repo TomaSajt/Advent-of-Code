d321ec60a059cb8588bc6270aa4a86e1932dbaad
Toma <tomasajtofficial@gmail.com>
2022-12-06 06:17:55 +0100

    refactor 2022 day6 cpp

--- cpp/2022/day6.cpp
+++ cpp/2022/day6.cpp
@@ -3,23 +3,16 @@ using namespace std;
 
 int main() {
     ifstream f("input.txt");
-    string s;
-    f >> s;
-    int n = s.size();
-    int sol1 = -1;
-    for (int i = 0; i < n; i++) {
-        set<char> se1, se2;
-        for (int j = 0; j < 4; j++) {
-            se1.insert(s[i + j]);
-        }
-        for (int j = 0; j < 14; j++) {
-            se2.insert(s[i + j]);
-        }
-        if (sol1 == -1 && se1.size() == 4) {
-            sol1 = i + 4;
-        }
-        if (se2.size() == 14) {
-            cout << sol1 << ' ' << i + 14 << endl;
+    string st;
+    f >> st;
+    int r1 = -1;
+    for (int i = 0; i < st.size(); i++) {
+        set<char> s1, s2;
+        for (int j = 0; j < 4; j++) s1.insert(st[i + j]);
+        for (int j = 0; j < 14; j++) s2.insert(st[i + j]);
+        if (r1 == -1 && s1.size() == 4) r1 = i + 4;
+        if (s2.size() == 14) {
+            cout << r1 << ' ' << i + 14 << endl;
             return 0;
         }
     }
