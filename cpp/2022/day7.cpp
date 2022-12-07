589cb48c011aae9416a04f25e74a5dda95427dd3
Toma <tomasajtofficial@gmail.com>
2022-12-07 06:47:14 +0100

    solve day 7 2022 in cpp

---
+++ cpp/2022/day7.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream f("input.txt");
+    string str;
+    vector<string> path;
+    map<string, int> sizes;
+    while (f >> str) {
+        if (str == "$") {
+            string cmd; f >> cmd;
+            if (cmd == "ls") continue;
+            string dir; f >> dir;
+            if (dir == "..") path.pop_back();
+            else if (dir == "/") path.clear();
+            else path.push_back(dir);
+        }
+        else {
+            string dmy;
+            getline(f, dmy);
+            if (str == "dir") continue;
+            int sz = stoi(str);
+            string crp;
+            for (auto& s : path) {
+                sizes[crp] += sz;
+                crp += "/" + s;
+            }
+            sizes[crp] += sz;
+        }
+    }
+    int ftot = 0;
+    for (auto [k, v] : sizes) {
+        if (v <= 100000) ftot += v;
+    }
+    int tot = sizes[""];
+    int sp = 70000000 - tot;
+    int trg = 30000000;
+    string best;
+    for (auto [k, v] : sizes) {
+        if (sp + v >= trg && sizes[best] > v) best = k;
+    }
+    cout << ftot << '\n' << sizes[best];
+}
