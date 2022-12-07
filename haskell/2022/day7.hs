ab03777ed6d1cb40a55d9bd3ded89e6fa79ae0b0
Toma <tomasajtofficial@gmail.com>
2022-12-07 22:59:18 +0100

    refactor 2022 day 7 haskell

--- haskell/2022/day7.hs
+++ haskell/2022/day7.hs
@@ -1,5 +1,8 @@
 import Data.List (foldl', tails)
-import qualified Data.Map as Map
+import qualified Data.Map as M
+
+type State = ([String], M.Map [String] Int)
+data Op = Dir String | Size Int 
 
 split _ []  = []
 split pred xxs@(x:xs) 
@@ -7,24 +10,20 @@ split pred xxs@(x:xs)
   | otherwise = ys : split pred rest
       where (ys, rest) = break pred xxs
 
-data Cmd = Cd String | Sz Int 
+next :: State -> Op ->  State
+next (path, szm) (Dir "/") = ([], szm)
+next (_:th, szm) (Dir "..") = (th, szm)
+next (path, szm) (Dir dir) = (dir : path, szm)
+next (path, szm) (Size num) = (path, foldl' (\a x -> M.insertWith (+) x num a) szm $ tails path)
 
-next :: ([String], Map.Map [String] Int) -> Cmd ->  ([String], Map.Map [String] Int)
-next (path, sizes) (Cd "/") = ([], sizes)
-next (_:xpth, sizes) (Cd "..") = (xpth, sizes)
-next (path, sizes) (Cd dir) = (dir : path, sizes)
-next (path, sizes) (Sz num) = (path, foldl' (\a x -> Map.insertWith (+) x num a) sizes $ tails path)
-
-readCmd :: [String] -> Cmd
-readCmd (('c':_:_:x):_) = Cd x
-readCmd (_:xs) = Sz $ sum . map (read . head . words) . filter (('d'/=) . head) $ xs
+readOp :: [String] -> Op
+readOp (('c':_:_:dir):_) = Dir dir
+readOp (_:l) = Size $ sum . map (read . head . words) . filter (('d'/=) . head) $ l
 
 main :: IO()
 main = do
     inp <- readFile "input.txt"
-    let cmds = map (readCmd . lines . tail) . split (=='$') $ inp
-    let (_,fs) = foldl' next ([], Map.empty) $ cmds
-    let res = Map.elems fs
-    let used = fs Map.! []
-    print $ sum . filter (<=100000) $ res
-    print $ minimum $ filter (>=used-40000000) res
+    let cmds = map (readOp . lines . tail) . split (=='$') $ inp
+    let (r:res) = M.elems . snd $ foldl' next ([], M.empty) $ cmds
+    print . sum . filter (<=100000) $ res
+    print . minimum $ filter (r-40000000<=) res
