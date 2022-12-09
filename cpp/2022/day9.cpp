e3a28edd93507478c48bdb685f03f141bc096cdb
Toma <tomasajtofficial@gmail.com>
2022-12-09 09:17:13 +0100

    refactor

--- cpp/2022/day9.cpp
+++ cpp/2022/day9.cpp
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define L 10
-
-int sign(int a) { return (a > 0) - (a < 0); }
 
 int main() {
     ifstream f("input.txt");
-    char op;
-    int d;
+    char op; int d;
     set<pair<int, int>> tailsPosSet;
-    vector<pair<int, int>> rope(L);
-    tailsPosSet.insert(rope[0]);
+    vector<pair<int, int>> rope(10);
+    tailsPosSet.insert({});
     while (f >> op >> d) {
         while (d--) {
-            if (op == 'R') rope[0].first++;
-            if (op == 'L') rope[0].first--;
-            if (op == 'U') rope[0].second++;
-            if (op == 'D') rope[0].second--;
-            for (int j = 1; j < L; j++) {
+            auto& [hx, hy] = rope[0];
+            if (op == 'R') hx++;
+            if (op == 'L') hx--;
+            if (op == 'U') hy++;
+            if (op == 'D') hy--;
+            for (int j = 1; j < 10; j++) {
                 auto [px, py] = rope[j - 1];
                 auto& [cx, cy] = rope[j];
-                int sx = sign(px - cx), sy = sign(py - cy);
-                int dx = abs(px - cx), dy = abs(py - cy);
-                if (dx > 1 || dy > 1) cx += sx, cy += sy;
+                if (abs(px - cx) > 1 || abs(py - cy) > 1) cx += (px > cx) - (px < cx), cy += (py > cy) - (py < cy);
             }
             tailsPosSet.insert(rope.back());
         }
